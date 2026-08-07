936813a81086ed74cca8f614ed90f6cfeabadf0b
Mark Kinyanjui <gitaumark502@gmail.com>
2026-08-07 20:27:11 +0300

    update the _init_ python file

--- agent/exceptions/__init__.py
+++ agent/exceptions/__init__.py
@@ -8,28 +8,37 @@ from agent.exceptions.adapter import (
     SerializationError,
     UnknownActionError,
 )
+from agent.exceptions.animal import AnimalError
 from agent.exceptions.base import KaggricultureError
 from agent.exceptions.configuration import ConfigurationError
+from agent.exceptions.crop import CropError
 from agent.exceptions.inventory import InventoryError
 from agent.exceptions.market import MarketError
 from agent.exceptions.movement import MovementError
+from agent.exceptions.observation import ObservationError
+from agent.exceptions.planning import PlanningError
 from agent.exceptions.strategy import StrategyError
 from agent.exceptions.validation import ValidationError
+from agent.exceptions.worker import WorkerError
 
 __all__ = [
     "AdapterError",
+    "AnimalError",
     "CompatibilityError",
     "ConfigurationError",
+    "CropError",
     "InvalidCoordinateError",
     "InventoryError",
     "KaggricultureError",
     "MarketError",
     "MissingFieldError",
     "MovementError",
-    "ObservationParseError",
+    "ObservationError",
+    "PlanningError",
     "SchemaValidationError",
     "SerializationError",
     "StrategyError",
     "UnknownActionError",
     "ValidationError",
+    "WorkerError",
 ]
